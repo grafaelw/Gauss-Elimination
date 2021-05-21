9336e0cd2a978eb87037334e6d688c404dd7cee9
Gregorius Rafael Widojoko <66363052+grafaelw@users.noreply.github.com>
2021-05-21 03:56:30 +0200

    Update gaussian.py

--- gaussian.py
+++ gaussian.py
@@ -1,5 +1,10 @@
 from Matrix import Matrix as mat
 import sys
+import unittest
+
+def augment(A,b):
+    A.addCol(b)
+    return A
 
 def linearSolve(A,b):
     # Reading number of unknowns
@@ -40,7 +45,23 @@ def linearSolve(A,b):
     # for i in range(n):
     #      print('X%d = %0.2f' %(i,x[i]), end = '\t')
 
+class GaussianTest(unittest.TestCase):
 
-def augment(A,b):
-    A.addCol(b)
-    return A
+    def AugmentTest(self):
+        A = mat.identity(4)
+        b = [2,3,4,5]
+        self.assertTrue(augment(A,b) == mat.fromList(list([1,0,0,0,2],[0,1,0,0,3],[0,0,1,0,4],[0,0,0,1,5])))
+
+    def linSolveTest(self):
+        # 3-variable Test
+        A = mat.fromList([[2,-4,5],[4,-1,0],[-2,2,-3]])
+        b = [-33,-5,19]
+        self.assertTrue(linearSolve(A,b) == list([-0.5,3,-4]))
+        # 4-variable Test
+        B = mat.fromList([[1,1,1,1],[-1,2,3,0],[2,3,-4,1],[1,1,2,-1]])
+        c = [13,-1,10,1]
+        self.assertTrue(linearSolve(B,c) == list([5,-0.25,1.5,6.75]))
+    
+
+if __name__=='__main__':
+    unittest.main()
